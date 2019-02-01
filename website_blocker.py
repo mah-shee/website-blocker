1189ede9f77a2cf5283b4b1faca3ead6003acc78
masatoshi0929 <kamata@bonx.jp>
2019-02-01 23:08:25 +0100

    first condition

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime as dt
 
+hosts_tmp = "hosts"
 hosts_path = r"/ect/hosts"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com", ]
@@ -8,6 +9,14 @@ website_list = ["www.facebook.com", "facebook.com", ]
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working hours")
+        with open (hots_temp, 'r+') as file:
+            content = file.read()
+            print(content)
+            for websiet in website_list:
+                if website in content:
+                    pass 
+                else:
+                    file.write(content)
     else:
         print("Fun hours")
     time.sleep(5)
